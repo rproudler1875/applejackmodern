65378a434da462d55a93cd45a47457eebbafd160
rproudler1875 <rproudler@hotmail.com>
2025-04-15 17:39:33 +0100

    Update ajmrp_init.lua

--- applejack_modernised/lua/autorun/ajmrp_init.lua
+++ applejack_modernised/lua/autorun/ajmrp_init.lua
@@ -3,6 +3,7 @@ if SERVER then
     
     -- Precache models
     util.PrecacheModel("models/props_c17/console01a.mdl")
+    util.PrecacheModel("models/weapons/v_hands.mdl") -- For keys
     
     -- Network strings
     util.AddNetworkString("AJMRP_CharacterCreation")
@@ -12,4 +13,10 @@ if SERVER then
     util.AddNetworkString("AJMRP_ProposeTrade")
     util.AddNetworkString("AJMRP_TradeRequest")
     util.AddNetworkString("AJMRP_AcceptTrade")
+    
+    -- Include weapon files
+    AddCSLuaFile("../entities/weapons/weapon_ajmrp_keys/cl_init.lua")
+    AddCSLuaFile("../entities/weapons/weapon_ajmrp_keys/shared.lua")
 end
+
+print("[AJMRP] ajmrp_init.lua loaded")
