3e941ee7fb0dfeaa60c9c89a33b6167990399036
rproudler1875 <rproudler@hotmail.com>
2025-04-15 18:04:36 +0100

    Update cl_init.lua

--- applejack_modernised/gamemode/cl_init.lua
+++ applejack_modernised/gamemode/cl_init.lua
@@ -18,13 +18,18 @@ function GM:HUDShouldDraw(name)
     return true
 end
 
--- Ensure spawn menu is initialized
+-- Debug spawn menu initialization
 hook.Add("PostGamemodeLoaded", "AJMRP_InitSpawnMenu", function()
-    print("[AJMRP] PostGamemodeLoaded: Checking g_SpawnMenu")
+    print("[AJMRP] PostGamemodeLoaded: Checking spawn menu")
     if g_SpawnMenu then
-        print("[AJMRP] g_SpawnMenu is available")
+        print("[AJMRP] g_SpawnMenu exists")
     else
-        print("[AJMRP] WARNING: g_SpawnMenu is nil")
+        print("[AJMRP] g_SpawnMenu is nil")
+    end
+    if g_ContextMenu then
+        print("[AJMRP] g_ContextMenu exists")
+    else
+        print("[AJMRP] g_ContextMenu is nil")
     end
 end)
 
@@ -34,21 +39,32 @@ hook.Add("PlayerBindPress", "AJMRP_OpenSpawnMenu", function(ply, bind, pressed)
         print("[AJMRP] PlayerBindPress: +menu, pressed = " .. tostring(pressed))
         if pressed then
             gui.EnableScreenClicker(true) -- Show mouse cursor
+            if not g_SpawnMenu then
+                print("[AJMRP] g_SpawnMenu is nil, initializing")
+                CreateSpawnMenu() -- Create spawn menu if not initialized
+            end
             if g_SpawnMenu then
-                print("[AJMRP] Opening spawn menu")
+                print("[AJMRP] Opening g_SpawnMenu")
                 g_SpawnMenu:Open()
             else
-                print("[AJMRP] g_SpawnMenu is nil, trying spawnmenu.Activate()")
-                spawnmenu.Activate()
+                print("[AJMRP] Fallback: Opening g_ContextMenu")
+                if g_ContextMenu then
+                    g_ContextMenu:Open()
+                else
+                    print("[AJMRP] ERROR: Both g_SpawnMenu and g_ContextMenu are nil")
+                end
             end
         else
             gui.EnableScreenClicker(false) -- Hide mouse cursor
-            if g_SpawnMenu then
-                print("[AJMRP] Closing spawn menu")
+            if g_SpawnMenu and g_SpawnMenu:IsVisible() then
+                print("[AJMRP] Closing g_SpawnMenu")
                 g_SpawnMenu:Close()
+            elseif g_ContextMenu and g_ContextMenu:IsVisible() then
+                print("[AJMRP] Closing g_ContextMenu")
+                g_ContextMenu:Close()
             end
         end
-        return true -- Suppress default Q menu
+        return true -- Suppress default context menu
     end
 end)
 
