88cbd68392f1cea8d607b2f814fde58ccf20fcfe
rproudler1875 <rproudler@hotmail.com>
2025-04-15 15:18:42 +0100

    Update sh_config.lua

--- applejack_modernised/gamemode/core/sh_config.lua
+++ applejack_modernised/gamemode/core/sh_config.lua
@@ -14,10 +14,11 @@ AJMRP.Config.Jobs = {
     ["vigilante"] = { name = "Vigilante", xpThresholds = { 1200, 2200, 3200 } },
 }
 
--- Economy Items
+-- Economy Items with descriptions
 AJMRP.Config.Items = {
     pistol = {
         name = "Pistol",
+        description = "A small firearm for self-defense.",
         price = 200,
         weight = 2,
         entity = "weapon_pistol",
@@ -25,6 +26,7 @@ AJMRP.Config.Items = {
     },
     smg = {
         name = "SMG",
+        description = "A rapid-fire submachine gun for combat.",
         price = 350,
         weight = 3,
         entity = "weapon_smg1",
@@ -32,12 +34,14 @@ AJMRP.Config.Items = {
     },
     food = {
         name = "Food",
+        description = "A meal to restore your hunger.",
         price = 50,
         weight = 1,
         jobs = { "Citizen", "Gangster", "Paramedic" }
     },
     medkit = {
         name = "Medkit",
+        description = "A medical kit to heal injuries.",
         price = 100,
         weight = 2,
         entity = "ajmrp_medkit",
@@ -45,6 +49,7 @@ AJMRP.Config.Items = {
     },
     printer = {
         name = "Money Printer",
+        description = "A device that generates credits over time.",
         price = 500,
         weight = 5,
         entity = "ajmrp_printer",
